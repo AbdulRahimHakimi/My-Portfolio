a0940634d479996bf4f61f3a5637eed2779dc704
Abdul Rahim Hakimi <Abdulrahimhakimi180@gmail.com>
2026-07-30 08:20:35 +0430

    My portfolio basic form

---
+++ script.js
@@ -0,0 +1,5 @@
+// Script for adding interactivity
+document.querySelector('.contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Message Sent Successfully!');
+});
